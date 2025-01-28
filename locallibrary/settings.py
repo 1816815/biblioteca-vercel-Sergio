5b594309d2ce979dbb8e72b7a3a8e79936955bb3
1816815 <1816815@alu.murciaeduca.es>
2025-01-28 13:17:51 +0100

    settings separados

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,20 +22,6 @@ import os
 env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 load_dotenv(env_path)
 
-# Cargar la variable de entorno. El segundo argumento es
-# el valor que ha de tomarse cuando la variable no esté
-# definida.
-stage = os.getenv("MYPROJECT_STAGE", "development")
-if stage == "production":
-    # Producción.
-    from .settings_production import *
-elif stage == "development":
-    # Desarrollo.
-    from .settings_dev import *
-else:
-    # Arrojar un error si MYPROJECT_STAGE tiene un valor desconocido.
-    raise ValueError("Unknown stage: {stage}")
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -180,3 +166,18 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Cargar la variable de entorno. El segundo argumento es
+# el valor que ha de tomarse cuando la variable no esté
+# definida.
+stage = os.getenv("MYPROJECT_STAGE", "development")
+if stage == "production":
+    # Producción.
+    from .settings_production import *
+elif stage == "development":
+    # Desarrollo.
+    from .settings_dev import *
+else:
+    # Arrojar un error si MYPROJECT_STAGE tiene un valor desconocido.
+    raise ValueError("Unknown stage: {stage}")
+
